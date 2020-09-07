d09d83de91e5495e8da6048a5feb551745eac263
Viviana Rodriguez <cvrodriguez@vivianas-air.lan>
2020-09-07 18:40:24 +0200

    proyect kick off 🧨

---
+++ src/components/PxButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <button
+    @click="buttonclick"
+    class="bg-transparent hover:bg-green-700 text-green-700 font-semibold hover:text-white py-2 border border-green-500 hover:border-transparent rounded"
+  >
+    <beat-loader :loading="isLoading" :color="'#68d391'" :size="8" />
+    <p v-show="!isLoading">
+      <slot v-show="isLoading"></slot>
+    </p>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "PxButton",
+  props: {
+    isLoading: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  methods: {
+    buttonclick() {
+      this.$emit("custom-click");
+    }
+  }
+};
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex-col items-center">
+    <h1 class="text-gray-700 text-6xl">Platzi Exchange v1.0.0</h1>
+    <p class="text-gray-600 text-xl text-center">
+      Proyecto para obtener las cotizaciones de las cryptomonedas mas
+      importantes atraves<br />Este proyecto es utukizado en el curso de Vue.js
+      Basico de platzi
+    </p>
+    <router-link to="/home">Home</router-link>
+  </div>
+</template>
